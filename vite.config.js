90089f04fa96491ca0135e0e8b4a5a0d4eaf4bb0
twizz4u <tiwatayoaragbaiye@gmail.com>
2025-05-24 08:43:50 +0100

    save changes before deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  basae: "/Painting-Services/",
+  base: "/Painting-Services/",
   plugins: [react(), tailwindcss()],
 });
